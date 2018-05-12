78a14c36bb6e089e01d39ef21e839239958134af
astoorangi <astoorangi@outlook.com>
2018-05-13 01:32:59 +0200

    Added jquery & ChartJS; Start vote()

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+function vote(id, option) {     // Wird ausgeführt durch Buttons von Umfrage
+    $("#" + id).hide()              // Verstecke Antwortform
+    console.log(option)
+}
+
+function showChart(id) {        // Erstelle Chart in 
+
+}
