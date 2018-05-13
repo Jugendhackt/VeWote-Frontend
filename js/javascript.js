07e8316c2f5a9cfcf06243b67fefbf5b834ba31e
astoorangi <astoorangi@outlook.com>
2018-05-13 10:20:52 +0200

    a

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,13 @@
-function vote(id, option) {     // Wird ausgeführt durch Buttons von Umfrage
-    $("#" + id).hide()              // Verstecke Antwortform
+function vote(id, option) { // Wird ausgeführt durch Buttons von Umfrage
+    $("#" + id).hide() // Verstecke Antwortform
     console.log(option)
 }
 
-function showChart(id) {        // Erstelle Chart in 
+function showChart(id) { // Erstelle Chart in 
 
+}
+
+function filterText() {
+    console.log()
+    document.getElementById("dropdown-box").classList.toggle("show");
 }
